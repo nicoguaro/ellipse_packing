fb9c56381d259de7d1765ca0e058f82d61e4e975
nicoguaro <nicoguarin@gmail.com>
2015-12-08 20:20:58 -0500

    Add radii mean and standard deviation

--- examples/ellipses_FreeCAD.py
+++ examples/ellipses_FreeCAD.py
@@ -7,11 +7,12 @@ import os
 
 
 doc = FC.newDocument("ellipses")
-folder = os.path.dirname(__file__) + ".\.."
+folder = os.path.dirname(__file__) #+ "/.."
 fname = folder + "/vor_ellipses.txt"
 data = np.loadtxt(fname)
 shapes = []
 area = 0
+radii = []
 for ellipse in data:
     cx, cy, b, a, ang = ellipse
     ang = ang*np.pi/180
@@ -19,8 +20,10 @@ for ellipse in data:
     place.Rotation = (0, 0, np.sin(ang/2), np.cos(ang/2))
     place.Base = FC.Vector(100*cx, 100*cy, 0)
     ellipse = Draft.makeEllipse(100*a, 100*b, placement=place)
+    radii.append([100*a, 100*b])
     shapes.append(ellipse)
     area = area + np.pi*a*b*100*100
 
-print area, " ", area/500/70
+print "area: %g " % (area/500/70)
+print "radius: %g +/- %g" % (np.mean(radii), np.std(radii))
 part = Part.makeCompound(shapes)
